019c35111cb798e6d9fcd1af4cd5da9b60a2e6f6
Atila Rampazo <atila.rampazo@dextra-sw.com>
2021-12-13 13:57:21 -0300

    feat: containirizado nextJS

--- frente_loja/front/next.config.js
+++ frente_loja/front/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};

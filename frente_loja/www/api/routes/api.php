18e734498247c0f75e12f4554b824718f55429a6
Atila Rampazo <atila.rampazo@dextra-sw.com>
2021-12-13 11:20:17 -0300

    feat: commit da ultima aula

--- frente_loja/www/api/routes/api.php
+++ frente_loja/www/api/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/version',function(){
+    return response()->json(['versao'=>'1.0.0','status'=>'up'],200);
 });
